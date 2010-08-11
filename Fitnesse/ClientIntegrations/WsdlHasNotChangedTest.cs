76a5745eee9170b75f2b0105ff848bee78fece52
Ryan Bergman <ryber@geolearning.com>
2010-08-11 10:14:03 -0500

    Upgraded WSDL for IIS 2008

--- Fitnesse/ClientIntegrations/WsdlHasNotChangedTest.cs
+++ Fitnesse/ClientIntegrations/WsdlHasNotChangedTest.cs
@@ -1,22 +1,68 @@
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Xml;
+using fit;
+using fitlibrary;
 
 public class WsdlHasNotChangedTest : WebServiceTestBase
 {
-    public bool TheCurrentWsdlonTheServerMatchesTheLastKnownGoodWsdl()
+    public ArrayFixture TheCurrentWsdlonTheServerMatchesTheLastKnownGoodWsdl()
     {
         var lastKnownGoodWsdl = new XmlDocument();
         lastKnownGoodWsdl.Load("geonext.wsdl");
 
         var remoteWsdl = GetWsdlFromServer();
+		return new ResultRowFixture(GetChangeLog(lastKnownGoodWsdl.InnerXml, remoteWsdl.InnerXml));
 
-        return lastKnownGoodWsdl.InnerXml == remoteWsdl.InnerXml;
     }
 
-    private XmlDocument GetWsdlFromServer()
+	public class DiffMessage
+	{
+		public DiffMessage(string message)
+		{
+			Message = message;
+		}
+		public string Message;
+	}
+
+	public ICollection GetChangeLog(string old, string newbie)
+	{
+		var position = 0;
+		var result = new List<DiffMessage>();
+	
+		foreach (var s in old)
+		{
+			if(newbie[position] != s)
+			{
+				var orgLIne = old.Substring(0, position + 8);
+				var difLine = newbie.Substring(0, position + 8);
+				result.Add(new DiffMessage(string.Format("DIFF AT Position {0}!",position)));
+				result.Add(new DiffMessage(string.Format("OLD: {0}", orgLIne)));
+				result.Add(new DiffMessage(string.Format("NEW: {0}", difLine)));
+
+				ConsoleIt(result);
+
+				break;
+			}
+			position++;
+		}
+
+		return result;
+	}
+
+	private void ConsoleIt(List<DiffMessage> messages)
+	{
+		foreach (var message in messages)
+		{
+			Console.WriteLine(message.Message);
+		}
+	}
+
+	private XmlDocument GetWsdlFromServer()
     {
         var request = WebRequest.Create(GeoMaestroServices.Url);
         request.Credentials = GeoMaestroServices.Credentials;
@@ -35,4 +81,11 @@ public class WsdlHasNotChangedTest : WebServiceTestBase
         remoteWsdl.LoadXml(content);
         return remoteWsdl;
     }
+}
+
+public class ResultRowFixture : ArrayFixture
+{
+	public ResultRowFixture(ICollection theCollection) : base(theCollection)
+	{
+	}
 }
